cd1e81fee20113f7eea4ad0f2852115545945cc1
Raghav Verma <raghav.verma190@gmail.com>
2024-05-05 17:37:34 +0530

    Dealing with missing events(i.e. a service fails)

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,8 +7,10 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4002/posts');
-    setPosts(res.data);
+    const res = await axios.get('http://localhost:4002/posts').catch((err) => {
+      console.log(err.message);
+    });
+    setPosts(res ? res.data : {});
   };
 
   useEffect(() => {
